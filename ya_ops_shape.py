5e005f7b0a1e0e06416940b5c79b891cae840ca3
Aleks <yetanothermodder@pm.me>
2024-11-24 04:45:46 +0100

    name change

---
+++ ya_ops_shape.py
@@ -0,0 +1,362 @@
+import bpy
+import ya_utils as Utils
+
+from bpy.types import Operator
+from bpy.props import StringProperty
+
+# TODO, refactor into a single operator class
+
+class ApplyShapes(Operator):
+    bl_idname = "ya.apply_shapes"
+    bl_label = ""
+    bl_description = "Applies the chosen shape to the selected models. This overrides any custom shape values"
+    bl_options = {'UNDO'}
+    
+    def execute(self, context):
+        apply_mq = context.scene.ya_props.shape_mq_chest_bool
+        
+        torso = Utils.get_object_from_mesh("Torso").data.shape_keys.key_blocks
+        mannequin = Utils.get_object_from_mesh("Mannequin").data.shape_keys.key_blocks
+        
+        size = context.scene.ya_props.chest_shape_enum
+        category = Utils.get_chest_category(size)
+        shape_presets = Utils.get_shape_presets(size)
+        if apply_mq:
+            ApplyShapes.reset_shape_values("mq", category)
+            ApplyShapes.apply_shape_values("mq", category, shape_presets)
+            ApplyShapes.mute_chest_shapes(mannequin, category)
+        else:
+            ApplyShapes.reset_shape_values("torso", category)
+            ApplyShapes.apply_shape_values("torso", category, shape_presets)
+            ApplyShapes.mute_chest_shapes(torso, category)
+        
+        if apply_mq:
+            bpy.context.view_layer.objects.active = Utils.get_object_from_mesh("Mannequin")
+        else:  
+            bpy.context.view_layer.objects.active = Utils.get_object_from_mesh("Torso")
+        bpy.context.view_layer.update()
+        return {"FINISHED"}
+
+    def apply_shape_values(apply_target, category, shape_presets):
+        ya_props = bpy.context.scene.ya_props
+        for shape_key in shape_presets:
+            norm_key = shape_key.lower().replace(" ","").replace("-","")
+            category_lower = category.lower()
+
+            if norm_key == "sag" and category_lower == "large":
+                category_lower = "omoi"
+            
+            prop = f"key_{norm_key}_{category_lower}_{apply_target}"
+            if hasattr(ya_props, prop):
+                setattr(ya_props, prop, 100 * shape_presets[shape_key])
+            
+    def reset_shape_values(apply_target, category):
+        reset = Utils.get_shape_presets(category)
+        ya_props = bpy.context.scene.ya_props
+
+        for reset_key in reset:
+            norm_key = reset_key.lower().replace(" ","").replace("-","")
+            category_lower = category.lower()
+
+            if norm_key == "sag" and category_lower == "large":
+                category_lower = "omoi"
+            
+            prop = f"key_{norm_key}_{category_lower}_{apply_target}"
+            if hasattr(ya_props, prop):
+                setattr(ya_props, prop, 100 * reset[reset_key])
+                             
+    def mute_chest_shapes(apply_target, category):
+        category_mute_mapping = {
+            "Large": (True, True), 
+            "Medium": (False, True), 
+            "Small": (True, False),   
+        }
+
+        # Gets category and its bools
+        mute_medium, mute_small = category_mute_mapping.get(category, (True, True))
+
+        # Apply the mute states to the target
+        apply_target[Utils.get_chest_size_keys("Medium")].mute = mute_medium
+        apply_target[Utils.get_chest_size_keys("Small")].mute = mute_small
+
+    def mute_gen_shapes(apply_target, gen: str):
+        gen_mute_mapping = {
+            "Gen A": (True, True, True), 
+            "Gen B": (False, True, True), 
+            "Gen C": (True, False, True),   
+            "Gen SFW": (True, True, False),   
+        }
+
+        # Gets category and its bools
+        mute_b, mute_c, mute_sfw = gen_mute_mapping.get(gen, (True, True, True))
+
+        # Apply the mute states to the target
+        apply_target["Gen B"].mute = mute_b
+        apply_target["Gen C"].mute = mute_c
+        apply_target["Gen SFW"].mute = mute_sfw
+
+    def mute_leg_shapes(apply_target, size: str):
+        size_mute_mapping = {
+            "Melon": (True, True), 
+            "Skull": (False, True), 
+            "Mini": (True, False),   
+        }
+
+        # Gets category and its bools
+        mute_skull, mute_mini = size_mute_mapping.get(size, (True, True))
+
+        # Apply the mute states to the target
+        apply_target["Skull Crushers"].mute = mute_skull
+        apply_target["Mini"].mute = mute_mini
+
+        if not mute_mini:
+            apply_target["Hip Dips (for YAB)"].mute = True
+            apply_target["Less Hip Dips (for Rue)"].mute = True
+
+    def mute_nail_shapes(apply_target, nails: str):
+        nails_mute_mapping = {
+            "Long": (True, True, True), 
+            "Short": (False, True, True), 
+            "Ballerina": (True, False, True), 
+            "Stabbies": (True, True, False), 
+             
+        }
+        # Gets category and its bools
+        mute_short, mute_ballerina, mute_stabbies = nails_mute_mapping.get(nails, (True, True, True))
+
+        # Apply the mute states to the target
+        apply_target["Short Nails"].mute = mute_short
+        apply_target["Ballerina"].mute = mute_ballerina
+        apply_target["Stabbies"].mute = mute_stabbies
+    
+    def toggle_other(apply_target, key: str, not_legs=True):
+       
+        if not_legs:
+            if apply_target[key].mute:
+                apply_target[key].mute = False
+            else:
+                apply_target[key].mute = True
+
+        # Alt hips were a mistake
+
+        else:
+            if key == "Hip":
+                if apply_target["Hip Dips (for YAB)"].mute and apply_target["Less Hip Dips (for Rue)"].mute:
+                    if apply_target["Rue"].mute: 
+                        apply_target["Hip Dips (for YAB)"].mute = False
+                        apply_target["Less Hip Dips (for Rue)"].mute = True
+                    else:
+                        apply_target["Hip Dips (for YAB)"].mute = True
+                        apply_target["Less Hip Dips (for Rue)"].mute = False
+                else:
+                    apply_target["Hip Dips (for YAB)"].mute = True
+                    apply_target["Less Hip Dips (for Rue)"].mute = True
+
+
+            elif apply_target[key].mute:
+                if key == "Rue" and not apply_target["Hip Dips (for YAB)"].mute:
+                    apply_target["Hip Dips (for YAB)"].mute = True
+                    apply_target["Less Hip Dips (for Rue)"].mute = False
+                if key == "Rue Other":
+                    key = "Rue"
+                apply_target[key].mute = False
+            else:
+                if key == "Rue" and not apply_target["Less Hip Dips (for Rue)"].mute:
+                    apply_target["Hip Dips (for YAB)"].mute = False
+                    apply_target["Less Hip Dips (for Rue)"].mute = True
+                if key == "Rue Other":
+                    key = "Rue"
+                apply_target[key].mute = True
+
+
+class MESH_OT_YA_ApplyChestCategory(Operator):
+    bl_idname = "ya.apply_chest_category"
+    bl_label = ""
+    bl_description = "Changes size without affecting custom values"
+    bl_options = {'UNDO'}
+
+    key: StringProperty() # type: ignore
+    
+    def execute(self, context):
+        apply_mq = context.scene.ya_props.shape_mq_chest_bool
+
+        torso = Utils.get_object_from_mesh("Torso").data.shape_keys.key_blocks
+        mannequin = Utils.get_object_from_mesh("Mannequin").data.shape_keys.key_blocks
+ 
+        if apply_mq:
+            ApplyShapes.mute_chest_shapes(mannequin, self.key)
+        else:
+            ApplyShapes.mute_chest_shapes(torso, self.key)
+
+        
+        return {"FINISHED"}
+
+
+class MESH_OT_YA_ApplyLegSizes(Operator):
+    bl_idname = "ya.apply_legs"
+    bl_label = ""
+    bl_description = "Applies leg sizes"
+    bl_options = {'UNDO'}
+
+    key: StringProperty() # type: ignore
+
+    def execute(self, context):
+        apply_mq = context.scene.ya_props.shape_mq_legs_bool
+
+        legs = Utils.get_object_from_mesh("Waist").data.shape_keys.key_blocks
+        mannequin = Utils.get_object_from_mesh("Mannequin").data.shape_keys.key_blocks
+
+        if apply_mq:
+            ApplyShapes.mute_leg_shapes(mannequin, self.key)
+        else:
+            ApplyShapes.mute_leg_shapes(legs, self.key)
+
+        return {"FINISHED"}
+    
+
+class MESH_OT_YA_ApplyOther(Operator):
+    bl_idname = "ya.apply_other_option"
+    bl_label = ""
+    bl_description = "Applies selected option"
+    bl_options = {'UNDO'}
+
+    key: StringProperty() # type: ignore
+    target: StringProperty() # type: ignore
+
+    def execute(self, context):
+        apply_mq = context.scene.ya_props.shape_mq_legs_bool
+
+        if self.target == "Torso":
+            not_legs = True
+            target = Utils.get_object_from_mesh("Torso").data.shape_keys.key_blocks
+            
+        if self.target == "Legs":
+            not_legs = False
+            target = Utils.get_object_from_mesh("Waist").data.shape_keys.key_blocks
+
+        if self.target == "Hands":
+            not_legs = True
+            target = Utils.get_object_from_mesh("Hands").data.shape_keys.key_blocks
+
+        if self.target == "Feet":
+            not_legs = True
+            target = Utils.get_object_from_mesh("Feet").data.shape_keys.key_blocks
+
+        mannequin = Utils.get_object_from_mesh("Mannequin").data.shape_keys.key_blocks 
+        
+        if apply_mq:
+            if self.key == "Hip" and not mannequin["Mini"].mute:
+                self.report({"ERROR"}, "Mini not compatible with alternate hips!")
+                return {"CANCELLED"}
+            ApplyShapes.toggle_other(mannequin, self.key, not_legs)
+        else:
+            if self.key == "Hip" and self.target == "Legs" and not target["Mini"].mute:
+                self.report({"ERROR"}, "Mini not compatible with alternate hips!")
+                return {"CANCELLED"}
+            ApplyShapes.toggle_other(target, self.key, not_legs)
+
+        return {"FINISHED"}
+    
+
+class MESH_OT_YA_ApplyGen(Operator):
+    bl_idname = "ya.apply_gen"
+    bl_label = ""
+    bl_description = "Apply genitalia shape"
+    bl_options = {'UNDO'}
+
+    key: StringProperty() # type: ignore
+
+    def execute(self, context):
+        apply_mq = context.scene.ya_props.shape_mq_legs_bool
+
+        Legs = Utils.get_object_from_mesh("Waist").data.shape_keys.key_blocks
+        mannequin = Utils.get_object_from_mesh("Mannequin").data.shape_keys.key_blocks
+
+        if apply_mq:
+            ApplyShapes.mute_gen_shapes(mannequin, self.key)
+        else:
+            ApplyShapes.mute_gen_shapes(Legs, self.key)
+
+        return {"FINISHED"}
+    
+
+class MESH_OT_YA_ApplyNails(Operator):
+    bl_idname = "ya.apply_nails"
+    bl_label = ""
+    bl_description = "Applies nails shape"
+    bl_options = {'UNDO'}
+
+    key: StringProperty() # type: ignore
+
+    def execute(self, context):
+        apply_mq = context.scene.ya_props.shape_mq_other_bool
+
+        hands = Utils.get_object_from_mesh("Hands").data.shape_keys.key_blocks
+        mannequin = Utils.get_object_from_mesh("Mannequin").data.shape_keys.key_blocks
+
+        if apply_mq:
+            ApplyShapes.mute_nail_shapes(mannequin, self.key)
+        else:
+            ApplyShapes.mute_nail_shapes(hands, self.key)
+
+        return {"FINISHED"}
+    
+
+class MESH_OT_YA_ApplyVisibility(Operator):
+    bl_idname = "ya.apply_visibility"
+    bl_label = ""
+    bl_description = "Hides selected nails/claws"
+    bl_options = {'UNDO'}
+
+    key: StringProperty() # type: ignore
+    target: StringProperty() # type: ignore
+
+    def execute(self, context):
+        collection = bpy.context.view_layer.layer_collection.children
+        if self.key == "Nails" and self.target == "Feet":
+            
+            if collection["Feet"].children["Toenails"].exclude:
+                collection["Feet"].children["Toenails"].exclude = False
+            else:
+                collection["Feet"].children["Toenails"].exclude = True
+        
+        elif self.target == "Feet":
+
+            if collection["Feet"].children["Toe Clawsies"].exclude:
+                collection["Feet"].children["Toe Clawsies"].exclude = False
+            else:
+                collection["Feet"].children["Toe Clawsies"].exclude = True
+    
+        elif self.key == "Nails" and self.target == "Hands":
+
+            if collection["Hands"].children["Nails"].exclude:
+                collection["Hands"].children["Nails"].exclude = False
+            else:
+                collection["Hands"].children["Nails"].exclude = True
+
+        elif self.target == "Hands" and self.key == "Clawsies":
+
+            if collection["Hands"].children["Clawsies"].exclude:
+                collection["Hands"].children["Clawsies"].exclude = False
+            else:
+                collection["Hands"].children["Clawsies"].exclude = True
+
+
+        return {"FINISHED"}
+
+
+classes = [
+    ApplyShapes,
+    MESH_OT_YA_ApplyChestCategory,
+    MESH_OT_YA_ApplyOther,
+    MESH_OT_YA_ApplyLegSizes,
+    MESH_OT_YA_ApplyGen,
+    MESH_OT_YA_ApplyNails,
+    MESH_OT_YA_ApplyVisibility
+
+]
+
+
+
+
+
---
+++ ya_ops_ui.py
@@ -0,0 +1,159 @@
+import bpy
+import os
+import ya_utils as utils
+
+from bpy.types import Operator
+from bpy.props import StringProperty
+
+class UI_OT_YA_SetBodyPart(Operator):
+    bl_idname = "ya.set_body_part"
+    bl_label = "Select body slot to export."
+    bl_description = "The icons almost make sense"
+
+    body_part: StringProperty() # type: ignore
+
+    def execute(self, context):
+        # Update the export_body_part with the selected body part
+        context.scene.ya_props.export_body_slot = self.body_part
+        return {'FINISHED'}
+
+
+class UI_OT_YA_DirSelector(Operator):
+    bl_idname = "ya.dir_selector"
+    bl_label = "Select Folder"
+    bl_description = "Select file or directory. Hold Alt to open the folder"
+    
+    directory: StringProperty() # type: ignore
+    category: StringProperty() # type: ignore
+
+    def invoke(self, context, event):
+        actual_dir = getattr(context.scene.ya_props, f"{self.category}_directory", "")     
+
+        if event.alt and event.type == "LEFTMOUSE" and os.path.isdir(actual_dir):
+            os.startfile(actual_dir)
+        elif event.type == "LEFTMOUSE":
+            context.window_manager.fileselect_add(self)
+
+        else:
+             self.report({"ERROR"}, "Not a directory!")
+    
+        return {"RUNNING_MODAL"}
+    
+
+    def execute(self, context):
+        actual_dir_prop = f"{self.category}_directory"
+        display_dir_prop = f"{self.category}_display_directory"
+        selected_file = self.directory  
+
+        if os.path.isdir(selected_file):
+            setattr(context.scene.ya_props, actual_dir_prop, selected_file)
+            display_dir = utils.directory_short(selected_file, 3) 
+
+            setattr(context.scene.ya_props, display_dir_prop, display_dir)
+            self.report({"INFO"}, f"Directory selected: {selected_file}")
+        
+        else:
+            self.report({"ERROR"}, "Not a valid path!")
+        
+        return {'FINISHED'}
+    
+
+class UI_OT_YA_ConsoleToolsDirectory(Operator):
+    bl_idname = "ya.consoletools_dir"
+    bl_label = "Select File"
+    bl_description = "Use this to manually find the TexTools directory and select ConsoleTools.exe. Hold Alt to open the TexTools folder if already found"
+    
+    filepath: StringProperty() # type: ignore
+
+    def invoke(self, context, event):
+        textools = context.scene.ya_props.textools_directory
+
+        if event.alt and os.path.exists(textools):
+            os.startfile(textools)
+
+        elif event.type == 'LEFTMOUSE':
+            context.window_manager.fileselect_add(self)
+
+        else:
+             self.report({'ERROR'}, "Not a directory!")
+    
+        return {'RUNNING_MODAL'}
+
+    def execute(self, context):
+        selected_file = self.filepath
+
+        if os.path.exists(selected_file) and selected_file.endswith("ConsoleTools.exe"):
+            path_parts = selected_file.split(os.sep)
+            textools_folder = os.sep.join(path_parts[:-1])
+            context.scene.ya_props.textools_directory = textools_folder
+            context.scene.ya_props.consoletools_status = "ConsoleTools Ready!"
+            self.report({'INFO'}, f"Directory selected: {textools_folder}")
+        
+        else:
+            self.report({'ERROR'}, "Not a valid ConsoleTools.exe!")
+        
+        return {'FINISHED'}
+    
+
+class UI_OT_YA_PMPSelector(Operator):
+    bl_idname = "ya.pmp_selector"
+    bl_label = "Select Modpack"
+    bl_description = "Select a modpack. If selected, hold Alt to open the folder, hold Shift to open modpack"
+    
+    filepath: StringProperty() # type: ignore
+    category: StringProperty() # type: ignore
+
+    def invoke(self, context, event):
+        actual_file = context.scene.ya_props.loadmodpack_directory 
+
+        if event.alt and event.type == "LEFTMOUSE" and os.path.isfile(actual_file):
+            path_parts = actual_file.split(os.sep)
+            actual_dir = os.sep.join(path_parts[:-1])
+            print(actual_dir)
+            os.startfile(actual_dir)
+
+        elif event.shift and event.type == "LEFTMOUSE" and os.path.isfile(actual_file):
+            os.startfile(actual_file)
+
+        elif event.type == "LEFTMOUSE":
+            context.window_manager.fileselect_add(self)
+
+        else:
+             self.report({"ERROR"}, "Not a valid modpack!")
+    
+        return {"RUNNING_MODAL"}
+    
+    
+    def execute(self, context):
+        selected_file = self.filepath
+
+        if os.path.exists(selected_file) and selected_file.endswith(".pmp"):
+            
+            context.scene.ya_props.loadmodpack_directory = selected_file
+            display_dir = utils.directory_short(selected_file, 1) 
+            context.scene.ya_props.loadmodpack_display_directory = display_dir[:-4]
+
+            self.report({'INFO'}, f"{display_dir} selected!")
+        
+        else:
+            self.report({'ERROR'}, "Not a valid modpack!")
+        
+        return {'FINISHED'}
+    
+
+class UI_OT_YA_CopyToFBX(Operator):
+    bl_idname = "ya.directory_copy"
+    bl_label = "Copy Path"
+    bl_description = "Copies the export directory to your modpack directry. This should be where your FBX files are located"
+
+    def execute(self, context):
+        context.scene.ya_props.savemodpack_directory = context.scene.ya_props.export_directory 
+        return {'FINISHED'}
+
+classes = [
+    UI_OT_YA_SetBodyPart,
+    UI_OT_YA_DirSelector,
+    UI_OT_YA_ConsoleToolsDirectory,
+    UI_OT_YA_PMPSelector,
+    UI_OT_YA_CopyToFBX
+]
