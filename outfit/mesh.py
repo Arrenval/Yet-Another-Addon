1ffbcaf61fc0d55e96af5fc747464e8585b2e3e8
Aleks <yetanothermodder@pm.me>
2025-01-04 05:42:48 +0100

    Add special handling for data transfer modifiers.

--- outfit/mesh.py
+++ outfit/mesh.py
@@ -22,10 +22,10 @@ class TagBackfaces(Operator):
 
         self.obj = bpy.context.active_object
 
-        backface_material = [material.name for material in self.obj.data.materials if material.name.endswith(".BACKFACES")][0]
+        backface_material = [material.name for material in self.obj.data.materials if material.name.endswith(".BACKFACES")]
         
         if backface_material:
-            self.material = backface_material[:-10]
+            self.material = backface_material[0][:-10]
         else:
             self.material = self.obj.active_material.name
         
@@ -158,28 +158,45 @@ class CreateBackfaces(Operator):
                 groups[group] = part
         return int(groups[current_group])
     
-class ModifierActiveShape(Operator):
+class ModifierShape(Operator):
     bl_idname = "ya.apply_modifier"
     bl_label = "Backfaces"
-    bl_description = "Applies Deform Modifier to active shape key"
     bl_options = {'UNDO'}
 
     @classmethod
     def poll(cls, context):
-        return context.mode == "OBJECT"
+        obj = context.active_object
+        modifier:str = context.scene.outfit_props.shape_modifiers
+        return context.mode == "OBJECT" and modifier != "None"
+    
+    @classmethod
+    def description(cls, context, properties):
+        props = context.scene.outfit_props
+        obj = context.active_object
+        modifier:str = props.shape_modifiers
+        if modifier == "None":
+            return "Missing modifier"
+        if obj.modifiers[modifier].type == "DATA_TRANSFER":
+            return "Applies Data Transfer to current shape key mix"
+        else:
+            return "Applies Deform Modifier to active shape key"
 
     def execute(self, context):
         props = context.scene.outfit_props
         self.keep = props.keep_modifier
         obj = context.active_object
-        modifier = props.deform_modifiers
+        modifier:str = props.shape_modifiers
         key_name = obj.active_shape_key.name
 
-        self.apply_modifier(key_name, obj, modifier)
-        self.report({'INFO'}, "Modifier Applied to Shape.")
+        if obj.modifiers[modifier].type == "DATA_TRANSFER":
+            self.apply_data(obj, modifier)
+            self.report({'INFO'}, "Applied data transfer.")
+        else:
+            self.apply_deform(key_name, obj, modifier)
+            self.report({'INFO'}, "Modifier Applied to Shape.")
         return {'FINISHED'}
     
-    def apply_modifier(self, key_name:str, target:Object, modifier:str) -> None:
+    def apply_deform(self, key_name:str, target:Object, modifier:str) -> None:
         bpy.ops.object.modifier_apply_as_shapekey(keep_modifier=self.keep, modifier=modifier)
         bpy.ops.object.mode_set(mode='EDIT')
         bpy.ops.mesh.select_all(action='SELECT')
@@ -194,8 +211,16 @@ class ModifierActiveShape(Operator):
         key_index = target.data.shape_keys.key_blocks.find(key_name)
         target.active_shape_key_index = key_index
 
+    def apply_data(self, target:Object, modifier:str) -> None:
+        old_shape = target.active_shape_key_index
+        bpy.ops.object.shape_key_add(from_mix=True)
+        target.data.update()
+        bpy.ops.object.modifier_apply(modifier=modifier)
+        bpy.ops.object.shape_key_remove()
+        target.active_shape_key_index = old_shape
+
 CLASSES = [
     TagBackfaces,
     CreateBackfaces,
-    ModifierActiveShape
+    ModifierShape
 ]
