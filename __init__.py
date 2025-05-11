f1bae1eadb560a1dbe4bb5e801eb30f1652a10db
Aleks <yetanothermodder@pm.me>
2025-05-11 06:02:38 +0200

    Version 0.14.0.

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ bl_info = {
     "name": "Yet Another Addon",
     "author": "Aleks",
     "description": "Several tools to speed up XIV modding workflows.",
-    "version": (0, 13, 5),
+    "version": (0, 14, 0),
     "blender": (4, 2, 1),
     "category": "",
     }
