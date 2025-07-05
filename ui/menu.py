c1dee9f51904159ae7d94ac4436ebfd2b723a88c
Aleks <yetanothermodder@pm.me>
2025-07-05 16:45:55 +0200

    Don't show modifier button when no modifiers.

--- ui/menu.py
+++ ui/menu.py
@@ -1,6 +1,6 @@
-from bpy.types              import UILayout
+from bpy.types import UILayout, Context
 
-def menu_vertex_group_append(self, context):
+def menu_vertex_group_append(self, context: Context):
     layout: UILayout = self.layout
     layout.separator(type="LINE")
     layout.operator("ya.add_yas_vgroups", text= "Add YAS Groups")
@@ -8,10 +8,11 @@ def menu_vertex_group_append(self, context):
     layout.operator("ya.remove_empty_vgroups", text="Remove Empty Vertex Groups")
     layout.operator("ya.remove_select_vgroups", text= "Remove Selected and Adjust Parent").preset = "MENU"
 
-def draw_modifier_options(self, context):
+def draw_modifier_options(self, context: Context):
     layout: UILayout = self.layout
-    layout.separator(type="LINE")
-    layout.operator("ya.apply_modifier_sk", text="Apply with Shape Keys", icon="CHECKMARK")
+    if context.active_object.modifiers:
+        layout.separator(type="LINE")
+        layout.operator("ya.apply_modifier_sk", text="Apply with Shape Keys", icon="CHECKMARK")
   
     
    
