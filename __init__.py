6fd9c55da8a6c2d599d891e4daacfe53bb31f01e
Aleks <yetanothermodder@pm.me>
2025-07-16 01:12:36 +0200

    v0.21.1

--- __init__.py
+++ __init__.py
@@ -57,7 +57,7 @@ def register():
     preferences.register_menus()
 
     set_handlers()
-    bpy.types.Scene.ya_addon_ver = (0, 21, 0)
+    bpy.types.Scene.ya_addon_ver = (0, 21, 1)
     
 
 def unregister():
