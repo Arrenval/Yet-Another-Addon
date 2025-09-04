1e847f2c9140e9583a8a79c32c81eb7fa318a204
Aleks <yetanothermodder@pm.me>
2025-09-04 19:57:53 +0200

    Prevent Mesh ID errors.

--- xiv/io/model/com/scene.py
+++ xiv/io/model/com/scene.py
@@ -7,7 +7,7 @@ from .exceptions import XIVMeshIDError
 
 def get_mesh_ids(obj: Object) -> tuple[int, int]:
     try:
-        name_parts = obj.name.split(" ")
+        name_parts = obj.name.strip().split(" ")
         if re.search(r"^\d+\.\d+\s", obj.name):
             group = int(name_parts[0].split(".")[0])
             part  = int(name_parts[0].split(".")[1])
