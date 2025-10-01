24513d1a2aacb1cee42837e92f7756a96ba1e527
Aleks <yetanothermodder@pm.me>
2025-10-01 20:51:59 +0200

    v1.0.1

--- __init__.py
+++ __init__.py
@@ -57,7 +57,7 @@ def register():
     preferences.register_keymaps()
 
     props.set_handlers()
-    bpy.types.Scene.ya_addon_ver = (1, 0, 0)
+    bpy.types.Scene.ya_addon_ver = (1, 0, 1)
     
 
 def unregister():
