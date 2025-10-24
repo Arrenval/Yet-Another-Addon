195ced380abf611ec32bf79c40afbec8af3094d3
Aleks <yetanothermodder@pm.me>
2025-10-24 06:55:01 +0200

    Reorder variables.

--- xiv/io/model/com/helpers.py
+++ xiv/io/model/com/helpers.py
@@ -119,17 +119,15 @@ def normalised_int_array(float_array: NDArray) -> NDArray:
     base_values = np.floor(int_values).astype(np.int16)
     remainders  = 255 - base_values.sum(axis=1) 
     fractions   = int_values - base_values    
-    cols        = float_array.shape[1]
+    col_pos     = np.arange(float_array.shape[1])
+    incr_mask   = col_pos < remainders[:, None]
     
     sorted_col_indices = np.argpartition(
                                     -fractions, 
-                                    kth=np.arange(cols), 
+                                    kth=col_pos, 
                                     axis=1
                                 )
 
-    col_pos   = np.arange(cols)
-    incr_mask = col_pos < remainders[:, None] 
-    
     result    = base_values.copy()
     incr_pos  = sorted_col_indices[incr_mask]
     incr_rows = np.where(incr_mask)[0]
